9d2357b120f5fa54a2b87a76f909dc02304a80eb
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-05 09:30:47 +0600

    create a resume builder folder

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>resume-builder-app</title>
+      </Head>
+      <div>Heading 1</div>
+    </>
+  );
+}
