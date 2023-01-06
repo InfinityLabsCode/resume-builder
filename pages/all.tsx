623bd3d0ae2a554246013b19eaf638e027459188
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-06 16:01:21 +0600

    update folder

---
+++ pages/all.tsx
@@ -0,0 +1,13 @@
+export default function All() {
+  return (
+    <>
+      <div className="flex-auto">
+        <div className="max-w-3xl mx-auto pt-10 pb-12 px-8">
+          <div className="bg-white mt-8 mobile:p-6 p-5  overflow-visible rounded-md mb-20">
+            Nahid Hasan Nahid
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
