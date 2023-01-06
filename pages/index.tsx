ae412c612a35ff3fb4a7c7d825422e79d0570faf
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-06 21:59:18 +0600

    update

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <h1 className="text-3xl mt-60 text-green-500 font-bold text-center">Nahid Hasan Nahid!</h1>
+    </>
+  );
+}
