e9f43d240cec4e86a1add32365b032d92deeab23
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-06 16:01:21 +0600

    update folder

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>resume-builder</title>
+        <meta name="description" content="resume-builder App" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
